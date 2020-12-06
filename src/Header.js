529c834eb89579e9784cae7bdb9c6e6b6228add7
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-12-05 18:06:27 -0600

    added react-router-dom and checkout

--- src/Header.js
+++ src/Header.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
+    <Link to="/">
       <img
         className="header__logo"
         src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
         alt=""
       />
-
+      </Link>
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
