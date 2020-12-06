c79b2420ec61905020693917752aad3da4699697
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-12-05 18:14:13 -0600

    built subtotal and css file for it. installed . installed react-currency-format.

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Checkout.css";
-
+import Subtotal from "./Subtotal";
 function Checkout() {
     return (
         <div className='checkout'>
@@ -19,6 +19,7 @@ function Checkout() {
                 {/* {basketItem} */} 
             </div>
             <div className="checkout__right">
+            <Subtotal />
                 <h2>The subtotal will go here</h2>
             </div>
         </div>
