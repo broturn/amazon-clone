59297a99ad724b4c5aa0ae1ea753328e97ea6e32
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-12-19 17:53:07 -0600

    rendered products on checkout pagae and wired up the remove from basket button

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,29 +1,38 @@
-import React from 'react'
+import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
+
 function Checkout() {
-    return (
-        <div className='checkout'>
-            <div className="checkout__left">
-                <img className='checkout__ad' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668.jpg" alt="" />
-            </div>
-            <div>
-                <h2 className="checkout__title">
-                    Your Shopping Basket
-                </h2>
-                {/* {basketItem} */}
-                {/* {basketItem} */}
-                {/* {basketItem} */}
-                {/* {basketItem} */}
-                {/* {basketItem} */}
-                {/* {basketItem} */} 
-            </div>
-            <div className="checkout__right">
-            <Subtotal />
-                
-            </div>
-        </div>
-    )
+  const [{ basket }, dispatch] = useStateValue();
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <h2 className="checkout__title">Your Shopping Basket</h2>
+
+        {basket.map((item) => (
+          <CheckoutProduct
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+          />
+        ))}
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
