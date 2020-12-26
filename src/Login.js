69d362105a84b5bbadd16be6980650f08254edaf
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-12-26 16:21:12 -0600

    built out login page, styled it , and started to wire it up

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const signIn = e => {e.preventDefault() /// firebase login
+  }
+
+  const register = e => { e.preventDefault() ///firebase register
+  }
+
+
   return (
     <div className="login">
-    <Link to= '/'>
-      <img className='login__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' alt=''/>
-    </Link>  
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" value={email} onChange ={e => setEmail(e.target.value)} />
+
+          <h5>Password</h5>
+          <input type="password" value={password} onChange = {e => setPassword(e.target.value)}/>
+
+          <button type='submit' onClick = {signIn}className='login__signInButton' >Sign In</button>
+        </form>
+        <p>
+          By signing-in you agree to AMAZON FAKE CLONE Conditions of Use & Sale. Please
+          see our Privacy Notice, our Cookies Notice and our Internet-Based Ads
+          Notice
+        </p>
+        <button onClick = {register} className='login__registerButton'>Create you Amazon Account</button>
+      </div>
     </div>
   );
 }
 
 export default Login;
-
-
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCXh-I4cazlFRELDy-jjBVuKPh7VAQigm4",
+    authDomain: "clone-42755.firebaseapp.com",
+    databaseURL: "https://clone-42755.firebaseio.com",
+    projectId: "clone-42755",
+    storageBucket: "clone-42755.appspot.com",
+    messagingSenderId: "6333452384",
+    appId: "1:6333452384:web:add2617e53d0739227f305",
+    measurementId: "G-Y0JC0NTE03"
+  };
