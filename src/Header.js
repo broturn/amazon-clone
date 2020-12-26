5983fc610aa77bbaecb4d238b4144c2e5060a5d1
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-12-26 15:39:05 -0600

    Building login page

--- src/Header.js
+++ src/Header.js
@@ -6,17 +6,16 @@ import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 
 function Header() {
-  const [ { basket }, dispatch] = useStateValue();
-
+  const [{ basket }, dispatch] = useStateValue();
 
   return (
     <div className="header">
-    <Link to="/">
-      <img
-        className="header__logo"
-        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
       </Link>
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -24,10 +23,12 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">Sign In</span>
-        </div>
+        <Link to='/login'>
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
@@ -38,10 +39,12 @@ function Header() {
         </div>
 
         <Link to="/checkout">
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
-        </div>
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
         </Link>
       </div>
     </div>
