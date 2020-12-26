404a0ec83f7cd5ef76461ddae414e88a9b9c9e4d
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-12-26 17:54:39 -0600

    added authentication support

--- src/Header.js
+++ src/Header.js
@@ -4,16 +4,23 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthenticaton = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
 
   return (
     <div className="header">
       <Link to="/">
         <img
           className="header__logo"
-          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
           alt=""
         />
       </Link>
@@ -23,10 +30,10 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <Link to='/login'>
-          <div className="header__option">
-            <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
+      <Link to={!user && "/login"}>
+          <div onClick={handleAuthenticaton} className="header__option">
+            <span className="header__optionLineOne">Hello {user?.email}</span>
+            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'} </span>
           </div>
         </Link>
         <div className="header__option">
