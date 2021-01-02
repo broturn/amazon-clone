e0d4976cdf81978a100d64b51bcbdc76e33c38c9
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-01-02 13:04:42 -0600

    updated header to display guest when no user signed in. added payment component and css page. put route in app.js

---
+++ src/Payment.js
@@ -0,0 +1,13 @@
+import { NfcRounded } from "@material-ui/icons";
+
+import React from 'react';
+import './Payment.css';
+function Payment() {
+    return (
+        <div className='payment'>
+            
+        </div>
+    )
+}
+
+export default Payment
